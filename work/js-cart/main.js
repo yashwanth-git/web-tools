d1fae0dc367b1a52ff498649078c7809fde4d168
yashwanth-sridharan <sridharan.y@northeastern.edu>
2022-06-25 14:57:02 -0400

    Adds basic setup with products

---
+++ work/js-cart/main.js
@@ -0,0 +1,26 @@
+"use strict";
+(function () {
+  const products = [
+    {
+      name: "Jorts",
+      img: "http://placekitten.com/150/150?image=1",
+      quantity: 1,
+      price: 0.99,
+      total: price * quantity,
+    },
+    {
+      name: "Jorts",
+      img: "http://placekitten.com/150/150?image=2",
+      quantity: 1,
+      price: 3.14,
+      total: price * quantity,
+    },
+    {
+      name: "Jorts",
+      img: "http://placekitten.com/150/150?image=3",
+      quantity: 1,
+      price: 2.73,
+      total: price * quantity,
+    },
+  ];
+});
