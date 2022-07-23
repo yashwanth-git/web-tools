144bd7bde2a1ee4c26697999880030a3e33e164c
yashwanth-sridharan <sridharan.y@northeastern.edu>
2022-07-23 15:10:47 -0400

    Adds Login from react-intro

--- work/react-services/src/App.jsx
+++ work/react-services/src/App.jsx
@@ -1,9 +1,14 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [username, setUsername] = useState('');
+  const [message, setMessage] = useState('');
   return (
     <div className="app">
-      <h1>Hello World!</h1>
+      <main>
+        
+      </main>
     </div>
   );
 }
---
+++ work/react-services/src/constants.js
@@ -0,0 +1,27 @@
+export const LOGIN_STATUS = {
+  PENDING: "pending",
+  NOT_LOGGED_IN: "notLoggedIn",
+  IS_LOGGED_IN: "loggedIn",
+};
+export const SERVER = {
+  AUTH_MISSING: "auth-missing",
+  AUTH_INSUFFICIENT: "auth-insufficient",
+  REQUIRED_USERNAME: "required-username",
+  REQUIRED_MESSAGE: "required-task",
+};
+
+export const CLIENT = {
+  NETWORK_ERROR: "networkError",
+  NO_SESSION: "noSession",
+};
+
+export const MESSAGES = {
+  [CLIENT.NETWORK_ERROR]:
+    "Trouble connecting to the network.  Please try again",
+  [SERVER.AUTH_INSUFFICIENT]:
+    "Your username/password combination does not match any records, please try again.",
+  [SERVER.REQUIRED_USERNAME]:
+    "Please enter a valid (letters and/or numbers) username",
+  [SERVER.REQUIRED_TASK]: "Please enter the task to do",
+  default: "Something went wrong.  Please try again",
+};
