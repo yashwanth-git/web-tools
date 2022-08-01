be893c1eb86267bfeee218db7990fa81a51edd26
yashwanth-sridharan <sridharan.y@northeastern.edu>
2022-08-01 11:37:18 -0400

    Adds init setup

---
+++ final/colors-hub/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(cookieParser());
+app.use(express.static("./build"));
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
