183566ba436cb51e06ef8cc6ef2bdd815f301a1b
yashwanth-sridharan <sridharan.y@northeastern.edu>
2022-08-01 13:05:37 -0400

    Adds services for login

---
+++ final/colors-hub/src/services.js
@@ -0,0 +1,51 @@
+export function fetchLogin(username) {
+  return fetch("/api/v1/session", {
+    method: "POST",
+    headers: new Headers({
+      "content-type": "application/json",
+    }),
+    body: JSON.stringify({ username }),
+  })
+    .catch(() => Promise.reject({ error: "networkError" }))
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      return response
+        .json()
+        .catch((error) => Promise.reject({ error }))
+        .then((err) => Promise.reject(err));
+    });
+}
+
+export function fetchSession() {
+  return fetch("/api/v1/session", {
+    method: "GET",
+  })
+    .catch(() => Promise.reject({ error: "networkError" }))
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      return response
+        .json()
+        .catch((error) => Promise.reject({ error }))
+        .then((err) => Promise.reject(err));
+    });
+}
+
+export function fetchLogout() {
+  return fetch("/api/v1/session", {
+    method: "DELETE",
+  })
+    .catch(() => Promise.reject({ error: "networkError" }))
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      return response
+        .json()
+        .catch((error) => Promise.reject({ error }))
+        .then((err) => Promise.reject(err));
+    });
+}
