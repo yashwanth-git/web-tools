b6bc15e7920bb22213709debaa8ee29a3955c373
yashwanth-sridharan <sridharan.y@northeastern.edu>
2022-08-14 19:48:04 -0400

    Changes the page limit

--- final/colors-hub/src/services.js
+++ final/colors-hub/src/services.js
@@ -91,7 +91,7 @@ export function fetchRemoveSavedColors(paletteId) {
     });
 }
 
-export function fetchColors(page = 1, limit = 10) {
+export function fetchColors(page = 1, limit = 8) {
   return fetch(`/api/v1/colors?page=${page}&limit=${limit}`, {
     method: "GET",
   })
